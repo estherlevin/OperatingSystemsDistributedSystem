01c317023bd8bb23ddcbf9bd020b93fadba728fa
Esther Levin <157747726+estherlevin@users.noreply.github.com>
2025-02-04 14:05:59 -0500

    Master file plus all master threads

---
+++ master/SendJobClientToMasterThread.java
@@ -0,0 +1,32 @@
+package master;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SendJobClientToMasterThread extends Thread {
+
+    private final BufferedReader requestReader;
+    private final ArrayList<String> jobs;
+
+    public SendJobClientToMasterThread(BufferedReader requestReader, ArrayList<String> jobs) {
+        this.requestReader = requestReader;
+        this.jobs = jobs;
+    }
+
+    @Override
+    public void run() {
+        try {
+            String job;
+            //if a job comes through (as a String) will add to arrayList which is shared memory location client-master
+            while ((job = requestReader.readLine()) != null) {
+                synchronized (jobs) {
+                    jobs.add(job);
+                    System.out.println("[Master] received job #ID: " + job.substring(1));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
